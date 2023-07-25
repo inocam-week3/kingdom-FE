48f1255f067b75d59fb1fe57353372cafc558cba
19Edwin92 <b_a_s_e@naver.com>
2023-07-25 21:31:36 +0900

    [Feat] Stories, StoriesComments API 생성(RTKquery)

--- src/redux/api/rtkquery.js
+++ src/redux/api/rtkquery.js
@@ -1,5 +1,5 @@
 import { instance } from "./instance";
-import { createApi } from "@reduxjs/toolkit/query/react"
+import { createApi } from "@reduxjs/toolkit/query/react";
 
 const axiosBaseQuery =
   () =>
@@ -25,35 +25,129 @@ const axiosBaseQuery =
     }
   };
 
-  export const heavenRTKQuery = createApi({
-    baseQuery: axiosBaseQuery(),
-    tagTypes: ["LOGIN"],
-    endpoints: (builder) => ({
-      // Login
-      loginRTK: builder.mutation({
-        query: (payload) => ({
-          url: "/api/auth/login",
-          method: "post",
-          data: payload,
-          types: "Auth",
-        }),
-      }),
-
-      // SignUp
-      signupRTK: builder.mutation({
-        query: (payload) => ({
-          url: "/api/auth/signup",
-          method: "post",
-          data: payload,
-          types: "Auth",
-        }),
+export const heavenRTKQuery = createApi({
+  baseQuery: axiosBaseQuery(),
+  tagTypes: ["LOGIN", "STORIES", "COMMENTS"],
+  endpoints: (builder) => ({
+    // Login
+    loginRTK: builder.mutation({
+      query: (payload) => ({
+        url: "/api/auth/login",
+        method: "post",
+        data: payload,
+        types: "Auth",
       }),
     }),
-  });
-  
-  export const {
-    useLoginRTKMutation,
-    useSignupRTKMutation,
 
-  } = heavenRTKQuery;
-  
+    // SignUp
+    signupRTK: builder.mutation({
+      query: (payload) => ({
+        url: "/api/auth/signup",
+        method: "post",
+        data: payload,
+        types: "Auth",
+      }),
+    }),
+
+    // STORIES - GET
+    getStoriesRTK: builder.useQuery({
+      query: () => ({
+        url: "/api/stories",
+        method: "get",
+        providesTags: ["STORIES"],
+      }),
+    }),
+    // STORIES - POST
+    postStoriesRTK: builder.mutation({
+      query: (payload) => ({
+        url: "/api/stories",
+        data: payload,
+        method: "post",
+        providesTags: ["STORIES"],
+      }),
+    }),
+    // STORIES - DELETE
+    deleteStoriesRTK: builder.mutation({
+      query: ({ id, payload }) => ({
+        url: `/api/stories/${id}`,
+        data: payload,
+        method: "delete",
+        providesTags: ["STORIES"],
+      }),
+    }),
+    // STORIES - PATCH
+    patchStoriesRTK: builder.mutation({
+      query: ({ id, payload }) => ({
+        url: `/api/stories/${id}`,
+        data: payload,
+        method: "patch",
+        providesTags: ["STORIES"],
+      }),
+    }),
+    // STORIES - Like
+    postStoriesLikeRTK: builder.mutation({
+      query: ({ id, payload }) => ({
+        url: `/api/stories/${id}/like`,
+        method: "post",
+        providesTags: ["STORIES"],
+      }),
+    }),
+
+    // STORIES_COMMENTS & 상세조회
+    getStoriesCOmmentsRTK: builder.useQuery({
+      query: ({ id, payload }) => ({
+        url: `/api/stories/${id}`,
+        method: "get",
+        providesTags: ["COMMENTS"],
+      }),
+    }),
+
+    // STORIES_COMMENTS - POST
+    postStoriesCOmmentsRTK: builder.mutation({
+      query: ({ id, payload }) => ({
+        url: `/api/stories/${id}/comments`,
+        data: payload,
+        method: "post",
+        providesTags: ["COMMENTS"],
+      }),
+    }),
+
+    // STORIES_COMMENTS - DELETE
+    deleteStoriesCOmmentsRTK: builder.mutation({
+      query: ({ postId, commentsId }) => ({
+        url: `/api/stories/${postId}/comments/${commentsId}`,
+        method: "delete",
+        providesTags: ["COMMENTS"],
+      }),
+    }),
+
+    // STORIES_COMMENTS - PATCH
+    patchStoriesCOmmentsRTK: builder.mutation({
+      query: ({ postId, commentsId, payload }) => ({
+        url: `/api/stories/${postId}/comments/${commentsId}`,
+        data: payload,
+        method: "patch",
+        providesTags: ["COMMENTS"],
+      }),
+    }),
+  }),
+});
+
+export const {
+  // Auth
+  useLoginRTKMutation,
+  useSignupRTKMutation,
+
+  // STORIES
+  useGetStoriesRTKQuery,
+  usePostStoriesRTKMutation,
+  useDeleteStoriesRTKMutaion,
+  usePatchStoriesRTKMutation,
+  usePostStoriesLikeRTKMutaion,
+
+  // STORIES_COMMENTS_DETAIL
+  useGetStoriesCOmmentsRTKQuery,
+  usePostStoriesCOmmentsRTKMutation,
+  useDeleteStoriesCOmmentsRTKMutation,
+  usePatchStoriesCOmmentsRTKMutation,
+} = heavenRTKQuery;
